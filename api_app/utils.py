68dbfc9e29d375f2961d0f25f79455fc216ca81a
rafal <buczynski.rafal2@gmail.com>
2022-04-15 15:30:21 +0200

    generate users in multiple threads

--- api_app/utils.py
+++ api_app/utils.py
@@ -1,8 +1,11 @@
 import os
 import csv
 import sqlite3
+import datetime
+import colorama
 from pathlib import Path
 from .factories import UserFactory
+from threading import Thread
 
 from .models import AnnualEnterpriseSurvey, User
 from .factories import UserFactory
@@ -31,9 +34,37 @@ def csv_to_db(csv_path, header_rows):
                     error_file.write(str(row) + '\n')
 
 
-def create_users(n=1000):
-    """Create users"""
+# def create_users(n=1000):
+#     """Create users"""
+#     for _ in range(n):
+#         user = UserFactory()
+#         user.save()
+
+
+def generate_users(n):
+    """Generate users"""
     for _ in range(n):
+        print(colorama.Fore.BLUE + '-- starting generate user')
         user = UserFactory()
+        print(colorama.Fore.YELLOW + '++ user has been created')
         user.save()
+        print(colorama.Fore.GREEN + '** user was saved')
+
+
+def create_users(n=100):
+
+    """Create threads"""
+    threads = [Thread(target=generate_users, args=(n,)) for _ in range(5)]
+
+    """t0 - will be used to calculate how long the function work"""
+    t0 = datetime.datetime.now()
+
+    """Starting threads"""
+    [t.start() for t in threads]
+
+    """Wait till all threads finish work."""
+    [t.join() for t in threads]
+
+    dt = datetime.datetime.now() - t0
+    print(colorama.Fore.GREEN + f"Working time: {dt.total_seconds():,.2f} sec.")
 
